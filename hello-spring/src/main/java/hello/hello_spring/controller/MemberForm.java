3923f45d20a027adaedd10bbaf218bd6b99be1fe
김구태 <gootae0220@naver.com>
2024-07-03 01:28:47 +0900

    chap 5 end

---
+++ hello-spring/src/main/java/hello/hello_spring/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package hello.hello_spring.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+    // alt + insert -> getter setter
+    public void setName(String name) {
+        this.name = name;
+    }
+}
